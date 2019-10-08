89f60a3f4a0182e9cb9036782aa045bbaac24c1e
DanielPri <privorotskyd@gmail.com>
2019-10-08 16:55:07 -0400

    Initial up to date with basics

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float verticalSpeed = 1;
+    [SerializeField] float horizontalSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontalDisplacement = Input.GetAxis("Horizontal") * verticalSpeed * Time.deltaTime;
+        float verticalDisplacement = Input.GetAxis("Vertical") * horizontalSpeed * Time.deltaTime;
+        transform.Translate(horizontalDisplacement, verticalDisplacement, 0);
+
+    }
+}
