33363b748f566053b5f7f85e616ccfb4abbdf458
DanielPri <privorotskyd@gmail.com>
2019-10-16 21:13:51 -0400

    Added gold and fixed movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject goldSpawns;
+    [SerializeField] GameObject goldBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform[] spawnLocations = goldSpawns.GetComponentsInChildren<Transform>();
+        foreach(Transform location in spawnLocations)
+        {
+            Debug.Log("spawning gold at: " + location.position.x + "," + location.position.y);
+            Object.Instantiate(goldBar, location.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] float verticalSpeed = 1;
-    [SerializeField] float horizontalSpeed = 1;
+    [SerializeField] float buoyantForce = 1f;
+    [SerializeField] float horizontalSpeed = 1f;
+    [SerializeField] float moveTimeDelay = 0.5f;
+
+    bool canMove = true;
+    float lastTimeMoved = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastTimeMoved = Time.time;
     }
 
     // Update is called once per frame
@@ -23,9 +27,57 @@ public class Player : MonoBehaviour
 
     private void MovePlayer()
     {
-        float horizontalDisplacement = Input.GetAxis("Horizontal") * verticalSpeed * Time.deltaTime;
-        float verticalDisplacement = Input.GetAxis("Vertical") * horizontalSpeed * Time.deltaTime;
-        transform.Translate(horizontalDisplacement, verticalDisplacement, 0);
+        float horizontalDisplacement = Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime;
+        transform.Translate(horizontalDisplacement, 0, 0);
+        if (Time.time - lastTimeMoved > moveTimeDelay) { 
+            if (canMove && (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1")))
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * buoyantForce);
+                lastTimeMoved = Time.time;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        HandleBoundaries(collider);
+        HandleGold(collider);
+    }
+
+    private void HandleGold(Collider2D collider)
+    {
+        if (collider.tag == "Gold")
+        {
+            Debug.Log("EEEEEEEEEEEEEY MACARAEENENA");
+            collider.transform.parent = transform;
+        }
+    }
+
+    private void HandleBoundaries(Collider2D collider)
+    {
+        if (collider.tag == "Sky")
+        {
+            canMove = false;
+        }
+        if (collider.tag == "Left")
+        {
+            Debug.Log("Player is in left");
+            transform.position = new Vector3(8.5f, transform.position.y, 0);
+        }
+        if (collider.tag == "Right")
+        {
+            Debug.Log("Player is in right");
+            transform.position = new Vector3(-9.2f, transform.position.y, 0);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Sky")
+        {
+            canMove = true;
+        }
+        
 
     }
 }
